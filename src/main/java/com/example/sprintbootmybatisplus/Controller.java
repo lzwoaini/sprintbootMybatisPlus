457f3145c91dc235beb443562c2d5828c55f3cd3
Ash <375928008@qq.com>
2022-12-02 16:00:56 +0800

    commit hjq function1and2

--- src/main/java/com/example/sprintbootmybatisplus/Controller.java
+++ src/main/java/com/example/sprintbootmybatisplus/Controller.java
@@ -19,5 +19,9 @@ public class Controller {
         //刘政的第一次更改
         // hjq的第一次(update)改 liuzheng update
         //liuzheng add row
+
+
+        //functionhjq1()
+        //functionhjq2()
     }
 }
